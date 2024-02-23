ca1703a5eb658da78dc855f321c61131a846066c
Robbert Broersma <git@robbertbroersma.nl>
2024-02-23 20:01:41 +0100

    build: create static build for Next.js project

--- packages/next-templates/next.config.js
+++ packages/next-templates/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
+  distDir: 'dist',
+  output: 'export',
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
@@ -28,3 +28,5 @@ module.exports = {
     return config;
   },
 };
+
+module.exports = nextConfig;
